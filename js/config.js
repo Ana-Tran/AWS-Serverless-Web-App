c6386d479dbd62731629921c53bfe9ab83fd2da7
Ana <anatran334@gmail.com>
2023-10-14 18:17:02 -0400

    re-add process.env

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 // test comment to deploy app
 window._config = {
     cognito: {
-        userPoolId: POOLID, // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: CLIENTID, // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: process.env.POOLID, // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: process.env.CLIENTID, // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: URL // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: process.env.URL // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
