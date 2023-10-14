828e38e4bbf0df801ad6b37348214127e93692ec
Ana <anatran334@gmail.com>
2023-10-14 15:17:53 -0400

    added cognito credentials

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ca-central-1_GVj8otfwW', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6v8fqr37bqjm7ju4elmhjqf0r1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
