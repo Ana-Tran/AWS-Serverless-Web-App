ae1ca2ce285c5a57a2383f1606de6401512653dd
Ana <anatran334@gmail.com>
2023-10-14 16:26:57 -0400

    update credentials to use .env variables

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ca-central-1_GVj8otfwW', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6v8fqr37bqjm7ju4elmhjqf0r1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: process.env.POOLID, // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: process.env.CLIENTID, // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://awgd4q0mk5.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: process.env.URL // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
