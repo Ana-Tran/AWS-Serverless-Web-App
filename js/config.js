975fa2c8783686560d511a8f8355f6426756c8ed
Ana <anatran334@gmail.com>
2023-10-14 18:11:33 -0400

    changes to env variable callback

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 // test comment to deploy app
 window._config = {
     cognito: {
-        userPoolId: process.env.POOLID, // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: process.env.CLIENTID, // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: POOLID, // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: CLIENTID, // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: process.env.URL // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: URL // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
